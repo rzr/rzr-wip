53b3436b41afead1b11db605bae37a942ab87d0e
Philippe Coval <p.coval@samsung.com>
2019-01-11 18:12:53 +0100

    WIP: mod: rules/devel/index.mk (sandbox/rzr/nuttx/devel/master)
    Change-Id: I6480a6f61aec0a6dfa5e33e1bae8f191d6378505
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- rules/devel/index.mk
+++ rules/devel/index.mk
@@ -3,8 +3,8 @@
 ref_file?=./nuttx/configs/stm32f746g-disco/nsh-ethernet/defconfig
 ref_file?=./nuttx/configs/stm32f769i-disco/nsh-ethernet/defconfig 
 
-rule/nuttx/diff: ${nuttx_dir}
-	meld ${ref_file} \
+#rule/nuttx/diff: ${nuttx_dir}
+#	meld ${ref_file} \
 
 
 rule/nuttx/meld: ./nuttx/configs/${nuttx_config}/defconfig
