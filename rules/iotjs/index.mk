4cc6f34d7e1df9b4df0892ccca8ac29766f417aa
Philippe Coval <p.coval@samsung.com>
2019-01-11 15:56:22 +0100

    WIP: mod: rules/iotjs/index.mk (sandbox/rzr/nuttx/devel/master)
    Change-Id: I5c993c1a4a435dc9934f3dddc945bc61003b99af
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- rules/iotjs/index.mk
+++ rules/iotjs/index.mk
@@ -1,6 +1,9 @@
 iotjs_machine?=${machine}
 #iotjs_machine?=stm32f4dis
 iotjs_dir=iotjs
+iotjs_config_dir?=iotjs/config/nuttx/${machine}
+iotjs_config_file?=${iotjs_config_dir}/config.default
+iotjs_nuttx_dir?=apps/system/iotjs
 IOTJS_ROOT_DIR="${iotjs_dir}"
 export IOTJS_ROOT_DIR
 IOTJS_ABSOLUTE_ROOT_DIR="${CURDIR}/${iotjs_dir}"
@@ -11,7 +14,8 @@ export IOTJS_ABSOLUTE_ROOT_DIR
 iotjs_url?=https://github.com/tizenteam/iotjs
 iotjs_branch?=sandbox/rzr/devel/nucleo-f767zi/master
 iotjs:
-	git clone --depth 1 --recursive -b ${iotjs_branch} ${iotjs_url} 
+	git clone --recursive -b ${iotjs_branch} ${iotjs_url}
+	@echo "TODO: --depth 1"
 	ls $@
 
 rule/iotjs/build/base: nuttx/.config
@@ -24,14 +28,14 @@ rule/iotjs/build/base: nuttx/.config
 iotjs/build/arm-nuttx/debug/lib/%: rule/iotjs/build
 	ls $@
 
-rule/iotjs/nuttx/build: nuttx/.config build/base iotjs/build
+rule/iotjs/nuttx/build: #nuttx/.config #build/base iotjs/build
 	which arm-none-eabi-gcc || sudo apt-get install gcc-arm-none-eabi
 	${MAKE} \
  IOTJS_ABSOLUTE_ROOT_DIR=${IOTJS_ABSOLUTE_ROOT_DIR} \
  IOTJS_ROOT_DIR=../${IOTJS_ROOT_DIR} \
  -C ${nuttx_dir}
 
-rule/iotjs/build: iotjs/config/nuttx/stm32f4dis/config.default ${nuttx_dir}
+rule/iotjs/build: ${iotjs_config_file} ${nuttx_dir}
 	cd iotjs && ./tools/build.py \
 --target-arch=arm \
 --target-os=nuttx \
@@ -39,6 +43,7 @@ rule/iotjs/build: iotjs/config/nuttx/stm32f4dis/config.default ${nuttx_dir}
 --target-board=${iotjs_machine} \
 --jerry-heaplimit=78
 
+#$ rm -rfv iotjs/build
 
 #${image_file}: build
 #	ls -l $@
@@ -51,16 +56,10 @@ rule/iotjs/build: iotjs/config/nuttx/stm32f4dis/config.default ${nuttx_dir}
 
 #all: prep configure build
 
-#run: deploy monitor
-
-#docker/run:
-#	docker-compose up ||:
-#	docker build -t "rzrwip_default" .
-#	docker run --privileged --rm -ti "rzrwip_default" run
-
 apps/system/iotjs: iotjs apps
 	@mkdir -p $@
 	cp -rf iotjs/config/nuttx/stm32f4dis/app/* $@/
+	make -C apps Kconfig TOPDIR=${CURDIR}/nuttx
 
 iotjs/meld: iotjs/config/nuttx/stm32f4dis/config.default nuttx/.config
 	$@ $^
@@ -97,4 +96,33 @@ todo/rule/iotjs/patch: \
 
 rule/build/iotjs: apps/system/iotjs menuconfig build
 
-rule/iotjs/devel: build rule/iotjs/patch rule/iotjs/patch rule/iotjs/build
+rule/iotjs/cleanall:
+	rm -rf iotjs/build
+
+rule/iotjs/devel: #build rule/iotjs/patch rule/iotjs/patch rule/iotjs/build
+	${MAKE} distclean
+	-rm -rfv ${iotjs_nuttx_dir}
+	${MAKE} rule/nuttx/configure
+	${MAKE} menuconfig
+	${MAKE} rule/nuttx/build
+	${MAKE} rule/iotjs/config
+	${MAKE} rule/iotjs/cleanall
+	${MAKE} rule/iotjs/build
+	${MAKE} apps/system/iotjs
+	-rm -rfv ${nuttx_config_file}
+	${MAKE} rule/nuttx/configure
+	${MAKE} rule/iotjs/menuconfig
+	${MAKE} rule/iotjs/nuttx/build
+
+# philippe@wsf1127:rzr-wip (sandbox/rzr/nuttx/devel/master %)$ mv apps/system/Kconfig  apps/system/Kconfig.mine
+
+rule/iotjs/config:
+	ls nuttx/.config
+	cp -av nuttx/.config ${iotjs_config_file}
+
+rule/iotjs/menuconfig: menuconfig
+	-grep CONFIG_IOTJS ${nuttx_config_file}
+	@echo 'CONFIG_IOTJS=y' >> ${nuttx_config_file}
+	grep PIPES ${nuttx_config_file}
+
+# uses VFP register arguments
