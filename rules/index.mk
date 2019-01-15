ef61e72b1159b1b13eecb89ec82b2f72e79dec4e
Philippe Coval <p.coval@samsung.com>
2019-01-15 18:32:36 +0100

    WIP: mod: rules/index.mk (sandbox/rzr/nuttx/devel/master)
    Change-Id: Icc986de39a19e161caacfb3ed140b209e76082f9
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- rules/index.mk
+++ rules/index.mk
@@ -45,8 +45,8 @@ nuttx_url?=file:///${HOME}/mnt/nuttx
 nuttx_branch?=sandbox/rzr/review/master
 #nuttx_branch=sandbox/rzr/devel/${machine}/master
 #nuttx_branch=sandbox/rzr/devel/stm32f7/master
-nuttx_config?=nucleo-144/f767-nsh
-#nuttx_config=nucleo-144/f767-netnsh
+#nuttx_config?=nucleo-144/f767-nsh
+nuttx_config=nucleo-144/f767-netnsh
 
 nuttx_config_file=${nuttx_dir}/.config
 nuttx_defconfig_file=${nuttx_dir}/configs/${nuttx_config}/defconfig
@@ -183,6 +183,10 @@ monitor: /dev/ttyACM0 # deploy
 build: rule/nuttx/build
 
 devel: rule/nuttx/menuconfig build deploy monitor rule/nuttx/savedefconfig
+
+
+rule/nuttx/diff: nuttx/defconfig ${nuttx_defconfig_file}
+	meld $<
 
 #include rules/iotjs/index.mk
 
