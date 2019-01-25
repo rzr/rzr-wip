3744fa98897718a6670ef93f44ee89b96d85ccab
Philippe Coval <p.coval@samsung.com>
2019-01-25 14:28:18 +0100

    WIP: add: example/webthing/index.js (sandbox/rzr/color/master)
    Change-Id: Iaf1beb92b7cdbd112ed0195b1d4e0aa62938b26e
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

---
+++ example/webthing/index.js
@@ -0,0 +1,32 @@
+/* -*- mode: js; js-indent-level:2;  -*-
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright 2018-present Samsung Electronics Co., Ltd. and other contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var webthing = require('webthing');
+
+function main () {
+  var port = process.argv[2]
+      ? Number(process.argv[2])
+      : 8889;
+
+  var thing = new webthing.Thing('ColorSensor', [], 'A RGB color sensor');
+  var server = new webthing.WebThingServer(new webthing.SingleThing(thing), port);
+  process.on('SIGINT', function () {
+    server.stop();
+  });
+  server.start();
+}
+
+main();
