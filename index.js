67535a75705e712b6963e11886082358feea5fb8
Philippe Coval <p.coval@samsung.com>
2019-01-28 23:31:24 +0100

    WIP: add: index.js (sandbox/rzr/webthing-iotjs/master)
    Change-Id: I4d57197ede3f3787383477805bf4070b056d3cd4
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

---
+++ index.js
@@ -0,0 +1,21 @@
+/* -*- mode: js; js-indent-level:2;  -*-
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright 2018-present Samsung Electronics Co., Ltd. and other contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var webthing = require('webthing-iotjs');
+
+var thing = new webthing.Thing('SomeThing');
+var server = new webthing.WebThingServer(new webthing.SingleThing(thing), 8888);
+server.start();
