d063c25601a15f1a3574f82d5f9878bd8dafd17b
Philippe Coval <p.coval@samsung.com>
2019-01-31 03:08:56 +0100

    WIP: mod: .eslintrc.js (sandbox/rzr/webthing-iotjs/mqtt/good/master)
    Change-Id: I61402f0de4a0651e53febd35f5306e55111b026d
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -96,7 +96,6 @@ module.exports = {
             "error",
             "bare-block"
         ],
-        "multiline-ternary": "error",
         "new-parens": "error",
         "newline-after-var": "off",
         "newline-before-return": "error",
@@ -178,7 +177,6 @@ module.exports = {
         "no-sync": "error",
         "no-tabs": "error",
         "no-template-curly-in-string": "error",
-        "no-ternary": "error",
         "no-throw-literal": "error",
         "no-trailing-spaces": "error",
         "no-undef-init": "error",
