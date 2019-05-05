13ef110866a239fdc30a161d1a7e5ca735c79f36
Philippe Coval <p.coval@samsung.com>
2019-05-05 23:26:50 +0200

    WIP: mod: rules/webthing-iotjs/index.mk (sandbox/rzr/nuttx/devel/master)
    Change-Id: I58be365f763b5577b6b0fcdc278d848ef09de784
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- rules/webthing-iotjs/index.mk
+++ rules/webthing-iotjs/index.mk
@@ -1,11 +1,13 @@
 rule/webthing-iotjs/deploy:
-	cd ${HOME}/public_html/tmp && \
-wget https://raw.githubusercontent.com/rzr/webthing-iotjs/master/webthing.js \
-wget https://raw.githubusercontent.com/rzr/webthing-iotjs/master/lib/express.js \
-wget https://raw.githubusercontent.com/rzr/webthing-iotjs/master/lib/property.js \
-wget https://raw.githubusercontent.com/rzr/webthing-iotjs/master/lib/server.js \ 
-wget https://raw.githubusercontent.com/rzr/webthing-iotjs/master/lib/thing.js \
-wget https://raw.githubusercontent.com/rzr/webthing-iotjs/master/lib/utils.js \
-wget https://raw.githubusercontent.com/rzr/webthing-iotjs/master/lib/value.js \
-wget https://raw.githubusercontent.com/rzr/webthing-iotjs/master/example/simplest-thing.js \
-	date
+	mkdir -p ${HOME}/public_html/tmp/wt
+	cd ${HOME}/public_html/tmp/wt && \
+wget https://raw.githubusercontent.com/rzr/webthing-iotjs/master/webthing.js && \
+wget https://raw.githubusercontent.com/SamsungInternet/iotjs-express/master/lib/express.js && \
+wget https://raw.githubusercontent.com/rzr/webthing-iotjs/master/lib/property.js && \
+wget https://raw.githubusercontent.com/rzr/webthing-iotjs/master/lib/server.js && \
+wget https://raw.githubusercontent.com/rzr/webthing-iotjs/master/lib/thing.js && \
+wget https://raw.githubusercontent.com/rzr/webthing-iotjs/master/lib/utils.js && \
+wget https://raw.githubusercontent.com/rzr/webthing-iotjs/master/lib/value.js && \
+wget https://raw.githubusercontent.com/rzr/webthing-iotjs/master/example/simplest-thing.js && \
+sync
+
