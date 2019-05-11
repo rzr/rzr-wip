ce51731134c8210d98c359316c0949a1069d6106
Philippe Coval <p.coval@samsung.com>
2019-05-11 17:26:11 +0200

    WIP: mod: rules/webthing-iotjs/pwm-property.js (sandbox/rzr/devel/robot/master)
    Change-Id: Ib0305549d8421cbe511b934326a61927322f8828
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- rules/webthing-iotjs/pwm-property.js
+++ rules/webthing-iotjs/pwm-property.js
@@ -58,8 +58,8 @@ function PwmOutProperty(thing, name, value, metadata, config) {
       self.port.setEnableSync(true);
 
       self.value.valueForwarder = function (value) {
-        if (typeof self.config.convert != undefined) {
-          value = self.config.convert(value);
+        if (typeof self.config.pwm.convert != undefined) {
+          value = self.config.pwm.convert(value);
         }
         verbose(self.port.freq);
         self.port.setDutyCycleSync(Number(value));
