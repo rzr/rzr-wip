398d9b8e2b3d73597802c19bd29bc61b0c99ea14
Philippe Coval <p.coval@samsung.com>
2019-05-27 18:29:44 +0200

    WIP: add: rules/twins/index.mk (sandbox/rzr/devel/robot/master)
    Change-Id: I636072e041a8771d745cd830168a86626ac035c7
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

---
+++ rules/twins/index.mk
@@ -0,0 +1,176 @@
+www_host?=192.168.1.12
+www_url?=http://${www_host}/~${USER}/${www_dir}
+www_dir?=t-d
+
+target_host?=192.168.1.13
+target_url?=http://${target_host}:8888
+twins_www_dir?=${HOME}/public_html/${www_dir}
+
+deploy_dir?=${twins_www_dir}
+#deploy_dir=${CURDIR}/tmp/deploy
+deploy_modules_dir=${deploy_dir}/iotjs_modules
+example_file=${deploy_dir}/index.js
+
+
+rule/twins/help:
+	@echo "# rule/twins/deploy/clean"
+	@echo "# rule/twins/prep"
+
+
+rule/twins/prep: rules/twins/rcS.template
+	ls $<
+
+rules/twins/rcS.template: rules/twins/rcS.template.in
+	sed -e "s|\$${base_url}|${www_url}|g" < $< > $@
+	cat $@
+
+
+
+rule/twins/www: ${twins_www_dir}
+	ls $^
+
+${twins_www_dir}:
+
+
+rule/twins/devel: rule/twins/prep rule/twins/www rule/iotjs/devel
+	sync
+
+#rule/twins/webpack: ${twins_www_dir}
+#	cd ${twins_www_dir} && ls package.json || npm init -y \
+#&& npm install --only=dev webpack-cli && npm install
+#	cd ${twins_www_dir} && npm run build
+
+rule/twins/property/%:
+	curl ${target_url}/properties/${@F}
+	curl -X PUT -d '{ "${@F}": ${value} }' ${target_url}/properties/${@F}
+	sleep 2
+
+
+rule/twins/test/%:
+	curl -X PUT -d '{ "${@F}": -90 }' ${target_url}/properties/${@F}
+	sleep 1
+	curl -X PUT -d '{ "${@F}": 90 }' ${target_url}/properties/${@F}
+	sleep 1
+
+rule/twins/test: \
+ rule/twins/test/Torso \
+ rule/twins/test/Shoulder \
+ rule/twins/test/Arm \
+ rule/twins/test/Hand \
+ #eol
+
+make?=make -f rules/twins/index.mk
+rule/twins/robot:
+	curl ${target_url}/properties
+	${make} ${@D}/property/Torso value=0
+	${make} ${@D}/property/Shoulder value=0
+	${make} ${@D}/property/Arm value=0
+	${make} ${@D}/property/Hand value=0
+	${make} ${@D}/Torso
+	${make} ${@D}/Shoulder
+	${make} ${@D}/Arm
+	${make} ${@D}/Hand
+
+rule/twins/%:
+	${make} rule/twins/property/${@F} value=0
+	${make} rule/twins/property/${@F} value=-90
+	${make} rule/twins/property/${@F} value=0
+	${make} rule/twins/property/${@F} value=90
+	${make} rule/twins/property/${@F} value=0
+
+rule/twins/Shoulder: #[ -90, 45]
+	${make} rule/twins/property/Shoulder value=-90
+	${make} rule/twins/property/Shoulder value=10
+	${make} rule/twins/property/Shoulder value=30
+	${make} rule/twins/property/Shoulder value=45
+	${make} rule/twins/property/Shoulder value=0
+
+rule/twins/Arm: # [-45 +45]
+	${make} rule/twins/property/${@F} value=0
+	${make} rule/twins/property/${@F} value=45
+	${make} rule/twins/property/${@F} value=-45
+	${make} rule/twins/property/${@F} value=0
+
+rule/twins/Hand: # [0 45]
+	${make} rule/twins/property/${@F} value=0
+	${make} rule/twins/property/${@F} value=40
+#	${make} rule/twins/property/${@F} value=10
+	${make} rule/twins/property/${@F} value=-5
+	${make} rule/twins/property/${@F} value=0
+
+
+rule/twins/demo:
+	${make} rule/twins/property/Hand value=0
+	${make} rule/twins/property/Hand value=20
+	${make} rule/twins/property/Arm value=15
+	${make} rule/twins/property/Shoulder value=-20
+	${make} rule/twins/property/Shoulder value=-40
+	${make} rule/twins/property/Shoulder value=-60
+	${make} rule/twins/property/Hand value=-5
+	${make} rule/twins/property/Shoulder value=45
+	${make} rule/twins/property/Arm value=10
+	${make} rule/twins/property/Arm value=-15
+
+
+${twins_dir}: rules/webthing-iotjs
+	rm -rf $@
+	cd ~/mnt/webthing-iotjs && make deploy deploy_modules_dir=${@}
+	cp -av $</*.js $@
+	cp -av $</*.json $@
+
+twins_url?=https://github.com/rzr/twins
+twins_dir?=twins
+
+twins_url=file:///home/${USER}/mnt/twins
+
+webpack_exe?=./node_modules/webpack-cli/bin/cli.js
+
+${twins_dir}:
+	mkdir -p ${@D}
+	git clone ${twins_url} --depth 1 $@
+
+
+twins/pack: ${twins_dir}
+	npm install webpack-cli
+	npm install webpack
+
+
+${deploy_modules_dir}: ${twins_dir}
+	make -C $< deploy deploy_modules_dir=$@
+
+rule/twins/deploy: ${deploy_modules_dir}
+	install rules/twins/index.js ${example_file}
+	du -ksc $<
+
+rule/twins/deploy/clean: ${deploy_modules_dir} rule/twins/deploy 
+	du -ksc $<
+	rm -rfv $</webthing-iotjs/example
+	du -ksc $<
+
+#twins/webpack: src
+#	${webpack_exe} # --context 
+
+webpack/help: ${webpack_exe}
+	${@} --help
+
+package.json:
+	npm init -y
+
+${webpack_exe}:
+	npm install webpack --save-dev
+	npm install webpack-cli --save-dev
+
+iotjs/start: ${example_file}
+#	iotjs $<
+	cd ${<D} && iotjs ${<F}
+
+sdcard_dir=/mnt/sdcard/
+
+rcS.template.in: ${deploy_dir}
+	cd ${deploy_dir}; \
+find . -type d \
+| while read dir; do \
+echo mkdir -p $${dir} ; \
+echo cd ${sdcard_dir}/$${dir} ; \
+echo "TODO: fetch files" ; \
+done
