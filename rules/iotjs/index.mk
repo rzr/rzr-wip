cb832229fef3eff5e616cc42c9322624b2e1a1f4
Philippe Coval <p.coval@samsung.com>
2019-06-14 10:50:54 +0200

    WIP: mod: rules/iotjs/index.mk (sandbox/devel/rzr/twins/master)
    Change-Id: I1d86839a422eea8b5cfc14be6afbb88b516348fc
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- rules/iotjs/index.mk
+++ rules/iotjs/index.mk
@@ -1,4 +1,13 @@
+##!/bin/make -f
+# -*- makefile -*-
+# SPDX-License-Identifier: MPL-2.0
+#{
+# Copyright 2018-present Samsung Electronics France SAS, and other contributors
 #
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.*
+#}
 
 iotjs_machine?=${machine}
 iotjs_machine_family?=stm32
@@ -16,23 +25,11 @@ export IOTJS_ABSOLUTE_ROOT_DIR
 
 iotjs_url?=https://github.com/Samsung/iotjs
 iotjs_branch?=master
-
-#TODO:
-#iotjs_url=https://github.com/tizenteam/iotjs
-#iotjs_branch=sandbox/rzr/review/master
-#iotjs_branch=sandbox/rzr/devel/master
-#iotjs_branch=sandbox/rzr/devel/${iotjs_machine}/master
-#iotjs_branch=sandbox/rzr/${iotjs_machine}/master
-#iotjs_url=file:///${HOME}/mnt/iotjs
-#iotjs_branch=sandbox/rzr/devel/${iotjs_machine}/master
-
 iotjs_lib_file?=iotjs/build/arm-nuttx/debug/lib/libiotjs.a
 iotjs_nuttx_config_file?=${nuttx_config_file}._iotjs.config
-
 nuttx_rc_file?=rules/iotjs/rcS.template
-#TODO
-#nuttx_rc_file=rules/webthing-iotjs/rcS.template
 nuttx_romfs_file?=${nuttx_dir}/configs/nucleo-144/include/nsh_romfsimg.h
+
 
 rule/iotjs/%:
 	${make} %
@@ -40,7 +37,6 @@ rule/iotjs/%:
 
 rule/iotjs/nuttx/%: nuttx/%
 	sync
-
 
 rule/iotjs/devel: rule/iotjs/base rule/iotjs/lib rule/iotjs/link deploy monitor
 	cp -a ${nuttx_dir}/defconfig rules/iotjs
@@ -60,8 +56,6 @@ ${nuttx_config_rc_file}: ${nuttx_rc_file}
 rule/iotjs/nuttx/build: ${iotjs_nuttx_config_file} ${nuttx_defconfig_file} ${nuttx_romfs_file}
 	cp -av $< ${nuttx_config_file}
 	${MAKE} rule/nuttx/build
-
-#arch/board/nsh_romfsimg.h
 
 rule/iotjs/link: ${iotjs_nuttx_config_file} ${iotjs_lib_file} ${nuttx_apps_dir}/system/iotjs ${nuttx_romfs_file}
 	cp -av $< ${nuttx_config_file}
@@ -84,26 +78,16 @@ iotjs/build/arm-nuttx/debug/lib/%: rule/iotjs/build
 rule/iotjs/config: ${iotjs_dir}
 	ls ${nuttx_dir}/.config
 	make -C ${nuttx_dir} savedefconfig
-#	-diff -u ${nuttx_dir}/defconfig ${iotjs_config_file}
-#	cp -av ${nuttx_dir}/defconfig ${iotjs_config_file}
 	cp ${nuttx_dir}/.config ${iotjs_dir}/config/nuttx/${iotjs_machine}/config.default
-#	grep -v '#' nuttx/.config
 
 rule/iotjs/configure: iotjs
-#	cp -av ${nuttx_dir}/defconfig ${iotjs_config_file}
 	-rm ${nuttx_apps_dir}/Kconfig
-#	-rm -rfv ${nuttx_config_file}
 	rm -rfv ${nuttx_dir}/.config
-#	${MAKE} rule/iotjs/cleanall
-#	${MAKE} rule/nuttx/configure
 	cp -av ${iotjs_config_file} ${nuttx_config_file}
-#	@echo 'CONFIG_IOTJS=y' >> ${nuttx_config_file}
 	${MAKE} menuconfig
 
 rule/iotjs/configured: ${nuttx_config_file}
 	grep 'CONFIG_NET_LOCAL=y' ${nuttx_config_file}
-#	grep 'CONFIG_NET_TCPBACKLOG=y' ${nuttx_config_file}
-#	-grep 'IPV6' ${nuttx_config_file}
 
 
 ${iotjs_nuttx_config_file}: ${nuttx_config_file}
@@ -127,14 +111,7 @@ rule/iotjs/nuttx/configure: ${iotjs_nuttx_config_file}
 	ls $<
 
 rule/iotjs/base: rule/iotjs/prep
-#	${MAKE} ${nuttx_dir}
-#	-${MAKE} distclean
-#	-rm -rfv ${iotjs_nuttx_dir}
-#	${MAKE} rule/nuttx/configure
 	${MAKE} rule/iotjs/nuttx/build
-#	${MAKE} deploy monitor # TODO
-#	${MAKE} rule/iotjs/config # TODO
-#	ls ${nuttx_include_file} #TODO rm
 
 rule/iotjs/lib: ${nuttx_include_file} ${nuttx_config_file}._iotjs.config 
 	cd ${iotjs_dir} && ./tools/build.py \
@@ -145,88 +122,24 @@ rule/iotjs/lib: ${nuttx_include_file} ${nuttx_config_file}._iotjs.config
  --jerry-heaplimit=78 \
  --profile=config/nuttx/${iotjs_machine}/nuttx.profile \
  #eol
-# --buildtype=debug
 
-
-#rule/iotjs/lib:
-#	rm -rf ${iotjs_dir}/build
-#	${MAKE} rule/iotjs/build
 
 ${iotjs_lib_file}: rule/iotjs/lib
 	ls $@
 
-#rule/iotjs/build/base: ${nuttx_dir}/.config
-
-
 rule/iotjs/menuconfig:
 	ls ${nuttx_config_file}
-#	${MAKE} menuconfig
-#	make -C ${nuttx_dir} savedefconfig
-# cp nuttx/defconfig iotjs/config/nuttx/nucleo-f767zi/
-#	-grep CONFIG_IOTJS ${nuttx_config_file}
-#	grep PIPES ${nuttx_config_file}
-
-#${nuttx_dir}/include/nuttx/config.h:
-#	grep -v CONFIG_IOTJS nuttx/.config > nuttx/base.config
-#	cp -av nuttx/base.config nuttx/.config
-#	make rule/nuttx/build
-
-#$ rm -rfv iotjs/build
-
-#${image_file}: build
-#	ls -l $@
-
-#prep: nuttx apps stlink patch
-#	sync
-
-#monitor: /dev/ttyACM0 deploy
-#	screen $< ${monitor_rate}
-
-#all: prep configure build
 
 ${nuttx_apps_dir}/system/iotjs: iotjs ${nuttx_apps_dir}
 	@mkdir -p $@
 	cp -rf ${iotjs_app_dir}/* $@/
 	make -C ${nuttx_apps_dir} Kconfig TOPDIR=${CURDIR}/${nuttx_dir}
 
-
 ${nuttx_apps_dir}/system/iotjs/%: ${nuttx_apps_dir}/system/iotjs
 	ls $@
 
-rule/iotjs/stm32/meld: iotjs/config/nuttx/${iotjs_reference_machine}/config.default ${nuttx_dir}/.config
-	ls ${<D}/..
-	${@F} $^
-
-rule/iotjs/tizenrt/meld: ${HOME}/mnt/TizenRT/build/configs/artik053s/iotjs/defconfig ${nuttx_dir}/.config
-	ls ${<D}/../..
-	${@F} $^
-
 rule/iotjs/clean:
 	rm -rf iotjs/build
-
-
-tmp/done/patch/iotjs/%: patches/iotjs/% iotjs
-	cd iotjs && git am ${CURDIR}/$<
-	mkdir -p ${@D}
-	touch $@
-
-tmp/done/patch/libtuv/%: patches/libtuv/% iotjs/deps/libtuv
-	cd iotjs/deps/libtuv && patch -p1 < ${CURDIR}/$<
-	mkdir -p ${@D}
-	touch $@
-
-patch/%: patches/% tmp/done/patch/%
-	wc -l $<
-
-rule/iotjs/patch:
-	echo TODO
-
-todo/rule/iotjs/patch: \
- tmp/done/patch/iotjs/0001-STM32F3-support.patch \
- tmp/done/patch/libtuv/0001-STM32F3-support.patch \
- # EOL
- # TODO: tmp/done/patch/iotjs/0002-wip.patch 
-	ls $^
 
 rule/build/iotjs: ${nuttx_apps_dir}/system/iotjs menuconfig build
 
@@ -237,14 +150,6 @@ rule/iotjs/cleanall:
 apps/system/Kconfig: ${apps_dir}/Kconfig ${apps_dir}/system/iotjs/Kconfig
 	ls $@
 
-# philippe@wsf1127:rzr-wip (sandbox/rzr/nuttx/devel/master %)$ mv apps/system/Kconfig  apps/system/Kconfig.mine
-
-
-
-todo:
-	cp ${nuttx_dir}/defconfig iotjs/config/nuttx/nucleo-f767zi/config.default
-	meld iotjs/config/nuttx/stm32f4dis/  iotjs/config/nuttx/nucleo-f767zi/
-
 rule/iotjs/meld: iotjs/config/nuttx/${iotjs_machine}/config.alloptions
 	meld $< ${nuttx_config_file}
 	meld $< ${nuttx_defconfig_file}
@@ -254,13 +159,6 @@ rule/iotjs/stm32f4dis: iotjs/config/nuttx/stm32f4dis/config.alloptions
 	meld $< ${nuttx_config_file}
 	meld $< ${nuttx_defconfig_file}
 
-#	ls iotjs/config/nuttx/${iotjs_machine}
-
-# uses VFP register arguments
-
-
-# iotjs/devel: menuconfig build run
-
 rule/iotjs/devel: rule/iotjs/base rule/iotjs/lib rule/iotjs/link deploy monitor
 	cp -a ${nuttx_dir}/defconfig rules/iotjs
 	-git diff
@@ -269,10 +167,3 @@ rule/iotjs/distclean:
 	rm -rf iotjs/build
 	find . -iname "*.obj" -exec rm -v {} \;
 	rm -rf ${nuttx_apps_dir}/system/iotjs/lib*.a
-
-#build rule/iotjs/patch rule/iotjs/patch rule/iotjs/build
-
-#rule/iotjs/meld: iotjs/config/nuttx/stm32f4dis/config.default 
-#	$@ $^
-
-devel: rule/iotjs/devel
