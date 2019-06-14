ff46eb4615f68ccac539874578ade9aa6e8ab1b7
Philippe Coval <p.coval@samsung.com>
2019-06-14 10:50:54 +0200

    WIP: mod: rules/devel/index.mk (sandbox/devel/rzr/twins/master)
    Change-Id: Ia3d764f2b6fb48034cd256d2177c464c15515854
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- rules/devel/index.mk
+++ rules/devel/index.mk
@@ -1,47 +1,15 @@
+#!/bin/make -f
+# -*- makefile -*-
+# SPDX-License-Identifier: MPL-2.0
+#{
+# Copyright 2018-present Samsung Electronics France SAS, and other contributors
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.*
+#}
+
 nuttx_dev_id?=066EFF323535474B43065221
-#nuttx_url=file:///${HOME}/mnt/nuttx
-#nuttx_branch=sandbox/rzr/devel/master
-#nuttx_branch?=sandbox/rzr/review/master
-# TODO
-#nuttx_url=https://github.com/TizenTeam/nuttx
-#nuttx_url=file:///${HOME}/mnt/nuttx
-#nuttx_branch=sandbox/rzr/devel/master
-#nuttx_branch=sandbox/rzr/devel/stm32f7nucleo/master
-# nuttx_branch?=sandbox/rzr/review/master
-# nuttx_branch=sandbox/rzr/devel/${machine}/master
-# nuttx_branch=sandbox/rzr/devel/stm32f7/master
-
-#ref_file?=./nuttx/configs/stm32f746g-disco/nsh-ethernet/defconfig
-#ref_file?=./nuttx/configs/stm32f769i-disco/nsh-ethernet/defconfig 
-
-#rule/nuttx/diff: ${nuttx_dir}
-#	meld ${ref_file} \
-
-
-rule/nuttx/meld: ./nuttx/configs/${nuttx_config}/defconfig
-	ls nuttx/.config
-	meld nuttx/.config $<
-
-#run: deploy monitor
-
-#docker/run:
-#	docker-compose up ||:
-#	docker build -t "rzrwip_default" .
-#	docker run --privileged --rm -ti "rzrwip_default" run
-
-
-rule/tizenrt/diff:
-
-
-rule/stm32/diff:
-	meld nuttx/configs/stm32f429i-disco  nuttx/configs/nucleo-144/
-
-
-rule/nucleo: \
- ${nuttx_dir}/configs/nucleo-144/f767-nsh/defconfig \
- ${nuttx_dir}/configs/nucleo-144/f767-netnsh/defconfig 
-	meld $^
-
 
 docker/run:
 	docker-compose up ||:
