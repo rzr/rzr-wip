24de1334191fe61933d3139ebdf6654254bec546
Philippe Coval <p.coval@samsung.com>
2019-06-17 12:37:08 +0200

    WIP: mod: rules/twins/adc.js (sandbox/devel/rzr/twins/master)
    Change-Id: I53cc011a3c2166d1464f48137fc7974376fbb893
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- rules/twins/adc.js
+++ rules/twins/adc.js
@@ -1,24 +1,27 @@
-console.log(process.iotjs);
 var adc = require('adc');
-var pins = require(process.iotjs.board).pin;
-console.log(pins);
-
+var board = process.iotjs.board;
+if (board) {
+  board = require(board);
+} else {
+  board = {};
+}
+  
 function start(pin)
 {
   this.pin = pin;
   this.config = {
     device: '/dev/adc0',
     direction: 'in',
-    pin: pins[pin]
+    pin: pin
   };
   var self = this;
   self.frequency = 1;
   self.period = 1000 / self.frequency;
-  
+
   setInterval(function () {
     console.log(self.pin + "=" + self.value);
   }, self.period);
-  
+
   this.port = adc.open(config, function (err) {
     console.log('opened');
     console.log(err);
@@ -27,27 +30,28 @@ function start(pin)
       return null;
     }
     console.log('ready');
-    self.inverval = setInterval(function () {
+    self.interval = setInterval(function () {
       self.value = Number(self.port.readSync());
     }, self.period);
   });
-
 }
 
 console.log(process.argv[2]);
-start(process.argv[2]];
-//start(pins.ADC1_1);
-//start(pins.ADC1_2);
-//start(pins.ADC1_3);
-// start(pins.ADC2_0);
-// start(pins.ADC2_1);
-// start(pins.ADC2_2);
-// start(pins.ADC2_3);
+var pin = process.argv[2] || 'ADC1_3';
+start(board.pin[pin]);
 
-//  "ADC1_15": 65798181,
-//  "ADC2_15": 99352613,
+// //start(pins.ADC1_1);
+// //start(pins.ADC1_2);
+// //start(pins.ADC1_3);
+// // start(pins.ADC2_0);
+// // start(pins.ADC2_1);
+// // start(pins.ADC2_2);
+// // start(pins.ADC2_3);
 
-//ADC3_15
+// //  "ADC1_15": 65798181,
+// //  "ADC2_15": 99352613,
 
-// cd /rom ; iotjs adc.js ADC1_0
-// cd /rom ; iotjs adc.js ADC2_3
+// //ADC3_15
+
+// // cd /rom ; iotjs adc.js ADC1_0
+// // cd /rom ; iotjs adc.js ADC2_3
