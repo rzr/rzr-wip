1a5c90f608330e4c9dafb8a7c824cbcd2a19d379
Philippe Coval <p.coval@samsung.com>
2019-06-21 12:51:01 +0200

    WIP: mod: rules/twins/index.mk (sandbox/rzr/devel/twins/master)
    Change-Id: Iafa2be231c77fa2f3feff65ba05776a8ef7de947
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- rules/twins/index.mk
+++ rules/twins/index.mk
@@ -63,7 +63,7 @@ rule/twins/deploy/clean: ${deploy_modules_dir} rule/twins/deploy
 	du -ksc $<
 
 rule/twins/romfs: ${nuttx_romfs_dir} ${twins_deploy_files}
-	${make} rule/twins/deploy nuttx_deploy_dir="$<"
+	${make} rule/twins/deploy deploy_modules_dir="$</iotjs-modules"
 	install ${twins_deploy_files} $</
 	rm -rfv ${nuttx_romfs_img_file}
 	${make} rule/nuttx/romfs.img
