d6bbc2e6370b0c56f4defec569b807808193a002
Philippe Coval <p.coval@samsung.com>
2019-06-21 14:45:55 +0200

    WIP: mod: rules/nuttx/index.mk (sandbox/rzr/devel/master)
    Change-Id: I93de17b1180cdb5967435f480007c0539cbb9ae0
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- rules/nuttx/index.mk
+++ rules/nuttx/index.mk
@@ -169,3 +169,13 @@ ${nuttx_config_rc_file}: ${nuttx_rc_file}
 ${nuttx_romfs_file}: ${nuttx_config_rc_file}
 	cd ${<D} && ../../../tools/mkromfsimg.sh -nofat  ../../..
 	ls -l $@
+
+
+${nuttx_config_rc_file}: ${nuttx_rc_file}
+	cp -av ${nuttx_rc_file} $@
+
+${nuttx_romfs_file}: ${nuttx_config_rc_file}
+	cd ${<D} && ../../../tools/mkromfsimg.sh -nofat  ../../..
+	ls -l $@
+${nuttx_config_rc_file}: ${nuttx_rc_file}
+	cp -av ${nuttx_rc_file}
