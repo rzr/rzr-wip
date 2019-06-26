afae236d7c1aa050c1e50d3a022dc1d0d76bbca2
Philippe Coval <p.coval@samsung.com>
2019-06-26 15:43:35 +0200

    WIP: mod: rules/iotjs/index.mk (sandbox/rzr/devel/master)
    Change-Id: Ie8d584e0c095a0c3d5b54491c81eb2a09a78d8c1
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- rules/iotjs/index.mk
+++ rules/iotjs/index.mk
@@ -37,7 +37,7 @@ iotjs_build_args+=--buildtype=debug
 iotjs_build_args+=--cmake-param="-DCMAKE_VERBOSE_MAKEFILE=ON"
 
 #TODO
-nuttx_rc_file?=rules/iotjs/rcS.template
+nuttx_rc_file?=rules/iotjs/rcS.template.sh
 
 IOTJS_ROOT_DIR="${iotjs_dir}"
 export IOTJS_ROOT_DIR
@@ -77,10 +77,10 @@ rule/iotjs/link: ${iotjs_nuttx_config_file} ${iotjs_lib_file} ${nuttx_apps_dir}/
  IOTJS_ROOT_DIR=../${IOTJS_ROOT_DIR} \
  -C ${nuttx_dir}
 
-rule/iotjs/prep: rules/iotjs/rcS.template ${nuttx_apps_dir}/system/iotjs/Kconfig
+rule/iotjs/prep: rules/iotjs/rcS.template.sh ${nuttx_apps_dir}/system/iotjs/Kconfig
 	ls $<
 
-rules/iotjs/rcS.template:
+rules/iotjs/rcS.template.sh:
 	echo "echo \"~~~ $(shell date -u)\"" > $@
 
 #TODO
