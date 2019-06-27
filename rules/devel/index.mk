042ce48d6c1dd76e2148efb389309e30d650eeaf
Philippe Coval <p.coval@samsung.com>
2019-06-27 20:32:58 +0200

    WIP: mod: rules/devel/index.mk (sandbox/rzr/devel/master)
    Change-Id: I5734dbf5065f06316c2f46048f79fa59553f8548
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- rules/devel/index.mk
+++ rules/devel/index.mk
@@ -10,6 +10,7 @@ iotjs_machine?=stm32f7nucleo
 
 nuttx_dev_id?=066EFF323535474B43065221
 nuttx_deploy_dir?=/media/${USER}/NODE_F767ZI1/
+#nuttx_branch?=master
 #nuttx_url?=file:///home/${USER}/mnt/nuttx/
 #nuttx_branch?=sandbox/rzr/devel/${iotjs_machine}/master
 #nuttx_branch?=sandbox/rzr/review/master
