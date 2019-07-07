d79abef9b4d042c05ed8ae64d72a37436d4a5dbd
Philippe Coval <p.coval@samsung.com>
2019-07-07 17:56:05 +0200

    WIP: mod: rules/iotjs/example.js (sandbox/rzr/devel/master)
    Change-Id: Ic0449c48fbca692c82612f02180f54657fb41fd0
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- rules/iotjs/example.js
+++ rules/iotjs/example.js
@@ -1,6 +1,9 @@
-console.log(process);
+// console.log(process);
 
-for (var arg in process.argv) {
-  arg = process.argv[arg];
-  console.log(arg);
-}
+// for (var arg in process.argv) {
+//   arg = process.argv[arg];
+//   console.log(arg);
+// }
+
+var now = new Date();
+console.log(Number(now));
