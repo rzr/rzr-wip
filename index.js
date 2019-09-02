b585d1cf12e3d95684313a9a1e6aef125b934210
Philippe Coval <p.coval@samsung.com>
2019-09-02 02:15:18 +0200

    WIP: mod: index.js (sandbox/rzr/webthing-iotjs/gaz/master)
    Change-Id: Ie4b18bb2823bb97f8c3343c62003d44f3b26b5ac
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- index.js
+++ index.js
@@ -14,6 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// TODO: MPL
+
 var console = require('console');
 var webthing = require('webthing-iotjs');
 var Property = webthing.Property;
@@ -21,130 +23,123 @@ var SingleThing = webthing.SingleThing;
 var Thing = webthing.Thing;
 var Value = webthing.Value;
 var WebThingServer = webthing.WebThingServer;
-
 var mqtt = require('mqtt');
+var verbose= console.log
 
-function MqttProperty(thing, value, config) {
+
+//TODO
+function baseName(str)
+{
+  var base = new String(str).substring(str.lastIndexOf('/') + 1); 
+    if(base.lastIndexOf(".") != -1)       
+        base = base.substring(0, base.lastIndexOf("."));
+   return base;
+}
+
+function MqttNumberProperty(thing, name, metadata, config) {
   var self = this;
-  self.config = config || {};
-  self.config.value = value || 0;
-  self.config.name = self.config.name || 'Unknown';
-  self.config.description = self.config.description || '?';
-  self.config.label = self.config.label || '?';
-  self.config.topic.type = self.config.topic.type || 'number';
-  if (self.config.topic.type === 'number') {
-    self.config.topic.propType = 'LevelProperty';
-  }
   self.config = config;
   Property.call(
-    this, thing, self.config.name,
-    new Value(self.config.value),
-    {
-      '@type': self.config.topic.propType,
-      description: self.config.description,
-      label: self.config.label,
-      readOnly: true,
-      type: self.config.topic.type
-    }
+    this,
+    thing,
+    name,
+    new Value(0),
+    metadata
   );
-  {
-    this.client = thing.parent.client;
+  verbose('log: connecting: ' + this.config.mqtt.connect.host);
 
-    var sub_topic = thing.parent.config.base_topic + self.config.topic.endPoint;
-    console.log('log: subscribing: ' + sub_topic);
-    console.log(thing.parent.config.subscribe);
-    this.client.subscribe(sub_topic, thing.parent.config.subscribe,
+  this.client = new mqtt.connect(this.config.mqtt.connect, function() {
+    verbose('log: connected, listening on port=' + self.config.mqtt.connect.port);
+    self.client.subscribe(self.config.mqtt.topic,
+                          self.config.mqtt.subscribe,
                           function(error) {
+                            verbose('log: subscribed: ' + self.config.mqtt.topic + ' error:' + error );
                             if (error) {
                               console.error('error: subscribe failed');
                               throw error;
                             }
-                          }
-                         );
-    this.client.on('message', function(data) {
-      if (data.topic === sub_topic) {
-        var object = JSON.parse(data.message.toString());
-        var updatedValue = object[self.config.topic.role];
-        if (self.config.topic.type === 'number') {
-          updatedValue = Number(updatedValue);
-        }
-        if (updatedValue !== self.lastValue) {
-          console.log('log: ' + self.getName() + ': change: ' + updatedValue);
-          self.value.notifyOfExternalUpdate(updatedValue);
-          self.lastValue = updatedValue;
-        }
+                          });
+    self.client.on('message', function(data) {
+      verbose('log: message: ' + String(data && data.topic));
+      verbose(data.message.toString());
+      var object = JSON.parse(data.message.toString());
+      var updatedValue = object[self.getName()];
+      verbose(updatedValue);
+      if (updatedValue !== self.lastValue) {
+        verbose('log: ' + self.getName() + ': change: ' + updatedValue);
+        self.value.notifyOfExternalUpdate(updatedValue);
+        self.lastValue = updatedValue;
       }
     });
-  }
+  });
 }
 
-function App () {
+function start() {
   var self = this;
 
-  var config = {
+  self.config = {
     port: 8888,
-    base_topic: 'io.github.rzr/rzr-wip/TODO/0',
     mqtt: {
-      host: 'localhost', // to update
-      port: 1883
+      connect: {
+        host: 'localhost',
+        port: 1883
+      },
+      subscribe: {
+        qos: 2,
+        retain: false
+      },
+      topic: '#',
+    },
+    property: {
+      name: "level",
+      metadata: {
+        description: "Level",
+        type: 'number',
+        readOnly: true,
+        '@type': 'LevelProperty'
+      }
     }
-  };
-
-  config.subscribe = {
-    retain: false,
-    qos: 2
   };
 
   if (typeof(process.argv[2]) != 'undefined') {
-    config.port = Number(process.argv[2]);
+    this.config.port = Number(process.argv[2]);
   }
 
   if (typeof(process.argv[3]) != 'undefined') {
-    config.base_topic = String(process.argv[3]);
+    this.config.mqtt.connect.host = String(process.argv[3]);
   }
 
   if (typeof(process.argv[4]) != 'undefined') {
-    config.mqtt.host = String(process.argv[4]);
+    this.config.mqtt.connect.port = Number(process.argv[4]);
   }
 
   if (typeof(process.argv[5]) != 'undefined') {
-    config.mqtt.port = Number(process.argv[5]);
+    this.config.mqtt.topic = String(process.argv[5]);
+    this.config.property.name = baseName(this.config.mqtt.topic);
   }
 
+  if (typeof(process.argv[6]) != 'undefined') {
+    this.config.property.name = String(process.argv[6]);
+  }
 
-  this.properties = [
-    {
-      name: 'Humidity',
-      description: 'Humidity Sensor',
-      label: 'Humidity (%)',
-      type: 'number',
-      topic: {
-        endPoint: '/air/humidity',
-        type: 'number',
-        role: 'humidity'
-      }
-    }
-  ];
-
-
-  this.config = config;
-  this.thing = new Thing('HumidityMqttSensor', ['MultiLevelSensor'], 'A set of sensors');
+  this.thing = new Thing('urn:TODO',
+                         'LevelMqttSensor',
+                         ['MultiLevelSensor'],
+                         'A level sensor');
   this.thing.parent = this;
 
-  console.log('log: connecting: ' + config.mqtt.host);
-  this.client = new mqtt.connect(config.mqtt, function() {
-    console.log('log: connected, listening on port=' + config.mqtt.port);
-    for (var property in self.properties) {
-      property = self.properties[property];
-      self.thing.addProperty(new MqttProperty(self.thing, null, property));
-    }
-  });
-  console.log('log: http serving on port=' + config.port);
-  this.server = new webthing.WebThingServer(new webthing.SingleThing(thing), Number(config.port));
+  this.thing.addProperty(new MqttNumberProperty(this.thing,
+                                          this.config.property.name,
+                                          this.config.property.metadata,
+                                          this.config
+                                         ));
+
+  verbose('log: http serving on port=' + config.port);
+  this.server = new webthing.WebThingServer(new webthing.SingleThing(thing), Number(self.config.port));
   process.on('SIGINT', function () {
     self.server.stop();
   });
   this.server.start();
 }
 
-App();
+start();
